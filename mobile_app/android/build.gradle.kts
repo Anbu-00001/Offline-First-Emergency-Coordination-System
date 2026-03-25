e1615a636d3762674ed641c83cb5cbbcadc44ffb
Anbu-00001 <anbuchelvanganesan.cse2024@citchennai.net>
2026-03-25 20:01:46 +0530

    chore: update Android build dependencies to fix CI mismatch

--- mobile_app/android/build.gradle.kts
+++ mobile_app/android/build.gradle.kts
@@ -1,17 +1,17 @@
 buildscript {
-    val kotlin_version by extra("1.9.22")
+    val kotlin_version by extra("1.9.0")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.3.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
 allprojects {
-    extra.set("kotlin_version", "1.9.22")
+    extra.set("kotlin_version", "1.9.0")
     repositories {
         google()
         mavenCentral()
